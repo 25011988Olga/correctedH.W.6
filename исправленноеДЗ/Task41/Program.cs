cb37b6dcb6d73ac159427356a39c0fcdbac7bc70
25011988Olga <olga.moseychuk.88@mail.ru>
2022-10-23 14:38:24 +0300

    Task41

---
+++ исправленноеДЗ/Task41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите кол-во элементов: ");
+int n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите эти элементы : ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0; 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше 0: {count}");
